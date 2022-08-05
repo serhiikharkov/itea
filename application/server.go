197b848b0b794dfe1e3a34c9a486d09f49a326e8
KSI <78685953+sergharkov@users.noreply.github.com>
2022-08-05 16:38:31 +0300

    Update server.go

--- application/server.go
+++ application/server.go
@@ -11,12 +11,11 @@ import (
 )
 
 func appHandler(w http.ResponseWriter, r *http.Request) {
-  htmlvar1 := "p"
   fmt.Println(time.Now(), "Hello from my new fresh server")
-
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+    htmlvar := "lkjlkjljlp"
     myOS, myArch := runtime.GOOS, runtime.GOARCH
     inContainer := "inside"
     if _, err := os.Lstat("/.dockerenv"); err != nil && os.IsNotExist(err) {
@@ -28,7 +27,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
     _, _ = fmt.Fprintf(w, "I'm running on %s/%s.\n", myOS, myArch)
     _, _ = fmt.Fprintf(w, "I'm running %s of a container.\n", inContainer)
     _, _ = fmt.Fprintf(w, "I'm running %s version container.\n",  os.Getenv("BUILD_NUMBER"))
-    _, _ = fmt.Fprintf(w, "This is my first Go project. Within ITEA courses .") 
+    _, _ = fmt.Fprintf(w, "This is my first Go project. Within ITEA courses %s.", htmlvar) 
     _, _ = fmt.Fprintf(w, "Sources you can find on my git https://github.com/sergharkov/itea .\n") 
     _, _ = fmt.Fprintf(w, "\n %s \n", os.Getenv("IMG_PRINT"))
 }
