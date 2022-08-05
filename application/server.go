9e46d9ad2d8e1f1aeb65c6996a4d8df114c13465
KSI <78685953+sergharkov@users.noreply.github.com>
2022-08-05 22:00:07 +0300

    Update server.go

--- application/server.go
+++ application/server.go
@@ -27,8 +27,8 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
     _, _ = fmt.Fprintf(w, "I'm running on %s/%s.\n", myOS, myArch)
     _, _ = fmt.Fprintf(w, "I'm running %s of a container. %s", inContainer, htmlvar)
     _, _ = fmt.Fprintf(w, "I'm running %s version container.\n",  os.Getenv("BUILD_NUMBER"))
-    _, _ = fmt.Fprintf(w, "This is my first Go project. Within ITEA courses. %s", htmlvar) 
-    _, _ = fmt.Fprintf(w, "Sources you can find on my git https://github.com/sergharkov/itea .\n") 
+    _, _ = fmt.Fprintf(w, "This is my first Go project. Within ITEA courses. %s", os.Getenv("HTML_P")) 
+    _, _ = fmt.Fprintf(w, "Sources you can find on my git https://github.com/sergharkov/itea .%s", os.Getenv("HTML_P") 
     _, _ = fmt.Fprintf(w, "\n %s \n", os.Getenv("IMG_PRINT"))
 }
 
