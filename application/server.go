5d57d10e50ecf89ddb6ad69efbd4a3f8827e1dc4
KSI <78685953+sergharkov@users.noreply.github.com>
2022-08-05 08:21:39 +0300

    Update server.go

--- application/server.go
+++ application/server.go
@@ -27,9 +27,13 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
     _, _ = fmt.Fprintf(w, "I'm running on %s/%s.\n", myOS, myArch)
     _, _ = fmt.Fprintf(w, "I'm running %s of a container.\n", inContainer)
     _, _ = fmt.Fprintf(w, "I'm running %s version container.\n",  os.Getenv("BUILD_NUMBER"))
-    _, _ = fmt.Fprintf(w, "This is my first Go project. Within ITEA courses .\n",  os.Getenv("BUILD_NUMBER")) 
-    _, _ = fmt.Fprintf(w, "Sources you can find on my git https://github.com/sergharkov/itea .\n",  os.Getenv("BUILD_NUMBER")) 
-      _, _ = fmt.Fprintf(w, "\n  \n",  os.Getenv("BUILD_NUMBER"))
+    _, _ = fmt.Fprintf(w, "This is my first Go project. Within ITEA courses .\n") 
+    _, _ = fmt.Fprintf(w, "Sources you can find on my git https://github.com/sergharkov/itea .\n") 
+      _, _ = fmt.Fprintf(w, "\n  <img class="shem"
+     src="https://github.com/sergharkov/itea/blob/main/infrastructure/draw.png?raw=true"
+     alt="ITEAN shem"
+     width: 50%
+     height: auto >  \n")
 }
 
 
