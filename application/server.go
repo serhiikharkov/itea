3a76776b606b493a83fb316880ff96b35f414ff7
KSI <78685953+sergharkov@users.noreply.github.com>
2022-08-05 16:29:58 +0300

    Update server.go

--- application/server.go
+++ application/server.go
@@ -17,6 +17,7 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+    htmlvar := "p"
     myOS, myArch := runtime.GOOS, runtime.GOARCH
     inContainer := "inside"
     if _, err := os.Lstat("/.dockerenv"); err != nil && os.IsNotExist(err) {
